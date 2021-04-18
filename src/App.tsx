04477393056ae5f4128e0154a3bdf6719cf00f07
ksby <kusa@db3.so-net.ne.jp>
2021-04-18 15:00:53 +0000

    src/App.css をクリアし、src/App.js を Tailwind CSS を利用して文字列を表示する内容に変更しました

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import {FC} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => (
+  <div className="pt-4 pl-4 text-red-600 text-4xl font-extrabold">
+    React+Tailwind CSS
+  </div>
+);
 
 export default App;
